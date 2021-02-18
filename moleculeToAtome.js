1c5bfb00928ef133e0b9ec4ddb9bd4e9e56a16f9
karim lemmouchi <karim.planb@gmail.com>
2021-02-18 15:56:36 +0100

    Add Parentheses() to check chemical atome with parentheses

--- moleculeToAtome.js
+++ moleculeToAtome.js
@@ -1,11 +1,11 @@
 function MoleculeToAtome() {
     var word = 'HMgPe4';
-    var wordParenthese = '(SO3)2';
+    var wordParenthese = '(HSO3)2';
     var wordHook = '[H(SO3)2]2';
     var obj = {};
 
     var regexParenthese = /(\((.*?)\))+.{1}/gm;
-    var arrayParentheses = [...word.matchAll(regexParenthese)];
+    var arrayParentheses = [...wordParenthese.matchAll(regexParenthese)];
     var regexHook = /\[(.*?)\]+.{1}/gm;
     var arrayHook = [...word.matchAll(regexHook)];
 
@@ -16,6 +16,9 @@ function MoleculeToAtome() {
     //     word = word.replace(element[0], '');
     // }
 
+    if (arrayParentheses.length > 0) {
+        Parentheses(arrayParentheses, obj);
+    }
     if (word.length > 0) {
         SimpleAtome(word, obj);
     }
@@ -23,16 +26,27 @@ function MoleculeToAtome() {
     return obj;
 }
 
-function SimpleAtome(word, obj) {
+function SimpleAtome(word, obj, numParenthese = 1) {
     var atomes = [...word.matchAll(/[A-Z]{1}[a-z]*[0-9]*/gm)];
     for (let element of atomes) {
         var numAtome = (/[0-9]/gm).exec(element[0]);
-        var letterAtome = (/[A-Z]{1}[a-z]{0,}/gm).exec(element[0]);
+        var letterAtome = (/[A-Z]{1}[a-z]{0,}/gm).exec(element[0])[0];
         var keys = Object.keys(obj);
         if (keys.includes(letterAtome)) {
-
+            obj[letterAtome] = numAtome !== null ? obj[letterAtome] + Number(numAtome[0]) : obj[letterAtome] + (1 * numParenthese);
         } else {
-            obj[letterAtome] = numAtome !== null ? Number(numAtome[0]) : 1;
+            obj[letterAtome] = numAtome !== null ? Number(numAtome[0]) * numParenthese : 1 * numParenthese;
+        }
+    }
+}
+
+function Parentheses(array, obj) {
+    for (let element of array) {
+        var numParenthese = Number(element[0][element[0].length - 1]);
+        var atomes = [...element[0].matchAll(/[A-Z]{1}[a-z]*[0-9]*/gm)];
+        for (let i = 0; i < atomes.length; i++) {
+            var atome = atomes[i][0];
+            SimpleAtome(atome, obj, numParenthese);
         }
     }
 }
