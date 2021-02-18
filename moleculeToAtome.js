62dfa11167a11e482d771ee0eb7d99a6518c9bf2
karim lemmouchi <karim.planb@gmail.com>
2021-02-18 15:15:50 +0100

    Create SimpleAtome() to refacroring

--- moleculeToAtome.js
+++ moleculeToAtome.js
@@ -17,18 +17,22 @@ function MoleculeToAtome() {
     // }
 
     if (word.length > 0) {
-        var atomes = [...word.matchAll(/[A-Z]{1}[a-z]*[0-9]*/gm)];
-        for (let atome of atomes) {
-            var numAtome = (/[0-9]/gm).exec(atome[0]);
-            var letterAtome = (/[A-Z]{1}[a-z]{0,}/gm).exec(atome[0]);
-            var keys = Object.keys(obj);
-            if (keys.includes(letterAtome)) {
-
-            } else {
-                obj[letterAtome] = numAtome !== null ? Number(numAtome[0]) : 1;
-            }
-        }
+        SimpleAtome(word, obj);
     }
 
     return obj;
+}
+
+function SimpleAtome(word, obj) {
+    var atomes = [...word.matchAll(/[A-Z]{1}[a-z]*[0-9]*/gm)];
+    for (let element of atomes) {
+        var numAtome = (/[0-9]/gm).exec(element[0]);
+        var letterAtome = (/[A-Z]{1}[a-z]{0,}/gm).exec(element[0]);
+        var keys = Object.keys(obj);
+        if (keys.includes(letterAtome)) {
+
+        } else {
+            obj[letterAtome] = numAtome !== null ? Number(numAtome[0]) : 1;
+        }
+    }
 }
